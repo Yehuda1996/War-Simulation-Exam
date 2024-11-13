9fcb7c2a2f9608589fc42d37d971d4422009b2db
Yehuda1996 <ygrajower@gmail.com>
2024-11-13 11:30:34 +0200

    types

---
+++ server/src/types/missilesType.ts
@@ -0,0 +1,7 @@
+export interface Missile {
+    name: string; 
+    description: string; 
+    speed: number; 
+    intercepts: string[]; 
+    price: number; 
+}
---
+++ server/src/types/organizationType.ts
@@ -0,0 +1,10 @@
+export interface Resource {
+    name: string; 
+    amount: number; 
+}
+
+export interface Organization {
+    name: string; 
+    resources: Resource[]; 
+    budget: number; 
+}
